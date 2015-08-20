c6e09a4f6734ff9096fe03a1074418a7bd51c2df
sergiolauar <sergiolauar@yahoo.com.br>
2015-08-20 15:11:49 -0700

    controller
    teste

---
+++ Allen20480/Allen20480/Controllers/ContatoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Allen20480.Controllers
+{
+    public class ContatoController : Controller
+    {
+        //
+        // GET: /Contato/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EnviaMensagem(string nome, string email, string mensagem)
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            return Json(new
+            {
+                Message = "OK"
+            });
+        }
+
+
+    }
+}
