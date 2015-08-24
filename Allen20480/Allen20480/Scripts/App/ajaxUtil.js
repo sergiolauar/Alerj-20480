eaa0ac207607d7e41b4328355a4f9ef1356ec8b2
sergiolauar <sergiolauar@yahoo.com.br>
2015-08-24 16:40:48 -0700

    teste

---
+++ Allen20480/Allen20480/Scripts/App/ajaxUtil.js
@@ -0,0 +1,54 @@
+﻿var Ajax = function (options) {
+
+    var _url = options.url;
+    var _type = options.type;
+    var _dataType = options.dataType;
+    var _contentType = options.contentType;
+    var _data = options.data;
+
+    this.makeRequest = function () {
+
+        return $.ajax({
+            url: _url,
+            type: _type,
+            dataType: _dataType,
+            contentType: _contentType,
+            data: _data,
+        }).promise();
+    };
+
+
+
+};
+
+
+var AjaxNovo = function (options) {
+
+    var _url = options.url;
+    var _type = options.type;
+    var _dataType = options.dataType;
+    var _contentType = options.contentType;
+    var _data = options.data;
+    var _success = options.success;
+    var _error = options.error;
+   
+
+    this.makeRequest = function () {
+
+         $.ajax({
+            url: _url,
+            type: _type,
+            dataType: _dataType,
+            contentType: _contentType,
+            data: _data,
+            success: _success,
+            error: _error                        
+            
+        });
+    };
+
+
+
+};
+
+
